df5b5d6680c09cedc86af9eef8e987da116ab3fb
Erik Van Vliet <erik.vanvliet@ontario.ca>
2022-06-08 14:15:30 -0400

    Add CrossOrigin annotation

--- src/main/java/ca/on/health/claim/pdf/template/templateservice/API/controllers/ServiceCodeController.java
+++ src/main/java/ca/on/health/claim/pdf/template/templateservice/API/controllers/ServiceCodeController.java
@@ -2,6 +2,7 @@ package ca.on.health.claim.pdf.template.templateservice.API.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(name = "/service")
 public class ServiceCodeController {
